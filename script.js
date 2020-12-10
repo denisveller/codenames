94f79d105d4dc166efeb755b0057f6fceacd2a07
Denis Veller <denis.veller@gmail.com>
2020-12-10 17:59:10 -0500

    indicators of cards

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ class Word {
         this.self = document.getElementById(this.id);
     }
     getWord() {
-        var temp = "<p>" + this.noun + "</p>" + "<p class='upside-down'>" + this.noun + "</p>";
+        var temp = "<p>" + this.noun + "</p>" + "<p class='upside-down'><i>" + this.noun + "</i></p>" + "<p class='corner'>"+this.noun+'</p>';
         this.self.innerHTML = temp;
     }
     cycleState() {
@@ -22,22 +22,30 @@ class Word {
         }
         switch (this.state) {
             case 0:
-                this.self.style.backgroundColor = "white";
+                this.self.style.backgroundColor = "wheat";
                 this.self.style.color = 'rgba(0,0,0,1)';
                 break;
             case 1:
                 this.self.style.backgroundImage = "url('blue.jpg')";
-                this.self.style.color = 'rgba(0,0,0,0)';
+                this.self.style.color = 'rgba(255,255,255,0)';
+                played[0] = played[0] + 1;
+                drawcards();
                 break;
             case 2:
+                played[0] = played[0] - 1;
+                played[1] = played[1] + 1;
                 this.self.style.backgroundImage = "url('red.jpg')";
+                drawcards();
                 break;
             case 3:
+                played[1] = played[1] - 1;
                 this.self.style.backgroundImage = "url('neutral.jpeg')";
+                drawcards();
                 break;
             case 4:
                 this.self.style.backgroundColor = "black";
                 this.self.style.backgroundImage = "url('')";
+                this.self.style.color = 'rgba(255,255,255,0)';
                 break;
         }
 
@@ -54,20 +62,45 @@ class Word {
 
 }
 var words = [];
+var card = [];
+var played = [0,0]; //blue, red
+function cards(x){
+    if (x === 0){
+        card = [9,8];
+    }
+    else{
+        card = [8,9];
+    }
+    btnarr = document.getElementsByClassName("playerBTN");
+    btnarr[0].style.display = 'none';
+    btnarr[1].style.display = 'none';
+    drawcards();
+}
+function reset(){
+    temp = document.getElementsByClassName("cards");
+    for (i = 0; i<temp.length; i++){
+        temp[i].style.display = 'none';
+    }
+}
+function drawcards(){
+    reset();
+    var blue = card[0] - played[0];
+    console.log(blue);
+    var red = card[1] - played[1];
+    console.log(red);
+    for (i = 0; i < blue; i++){
+        var temp = "b" + i;
+        document.getElementById(temp).style.display = 'block';
+    }
+    for (i = 0; i < red; i++){
+        var temp = "r" + i;
+        document.getElementById(temp).style.display = 'block';
+    }
+}
 function assign(file) {
     var wordsOnThePage = document.getElementsByClassName("word");
     for (var i = 0; i < wordsOnThePage.length; i++) {
         words.push(new Word(wordsOnThePage[i].id, file))
         words[i].getWord();
     }
-
-    // function randNouns(){
-    //     var r = Math.floor(Math.random()*file.length);
-    //     /*while (uArr.includes(r)){
-    //         r = Math.floor(Math.random()*file.length);
-    //     }*/
-    //     uArr.push(r);
-    //     var w = file[r];
-    //     return w;
-    // }
 }
